5325d77d118aefb2e7f415287333407ca2ef134d
Jikky0Meoow <132561214+Jikky0Meoow@users.noreply.github.com>
2026-05-14 11:54:08 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-dark-900 text-white overflow-x-hidden">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
